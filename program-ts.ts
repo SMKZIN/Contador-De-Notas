068175b1ac6b8ec1f61203107b8c98ffb6839aef
Samuel <samueldavi6306@gmail.com>
2024-07-05 17:50:59 -0300

    First Commit

---
+++ program-ts.ts
@@ -0,0 +1,33 @@
+// tela de inicio:
+const TELAINICIO: string = "<--- ## Contador de notas ## --->\n\n1 - Nota\n\n2 - Aprovado ou reprovado\n\n3 - Sair\n\n< --- ################################## --->"
+console.log(TELAINICIO )
+
+// opções:
+
+let readlineSync = require("readline-sync")
+let retornoOp1: string = "<--- ## Contador de notas ## --->\n\n1-Valor da Prova 1:\n\n2-Valor da Prova 2:"
+
+const opcao: any = readlineSync.question("Escolha uma opcao:")
+
+
+
+// condição da escolha:
+    if(opcao == 1){
+        console.log(`\n-Contador:Abrindo opção 1.... \n\n ${retornoOp1} `)
+
+    }else if(opcao == 2){
+        console.log("\n-Contador:Abrindo opção 2....")
+
+    }else{
+        console.log("\n-Contador:Saindo...")
+    }
+ // opcao 1 - Soma das notas da prova:       
+const nota1: number = parseInt(  readlineSync.question("\n\nNota Prova 1:\n"))
+const nota2: number = parseInt(readlineSync.question("Nota Prova 2:\n"))
+let somaN1eN2:number = nota1 + nota2
+const saidaNota: any  =`<--- ## Contador de notas ## --->\n\n-Valor da Prova 1: ${nota1} pontos\n\n-Valor da Prova 2: ${nota2} pontos\n\n#Sua nota total é: ${somaN1eN2} pontos` 
+console.log(saidaNota)
+
+
+ 
+
